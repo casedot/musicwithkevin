76ead8ce96cadaa8af493d7d535b6b141fe056d0
Mike <casedot@gmail.com>
2018-02-06 23:32:26 -0800

    minor fixes and tweeks

--- src/js/custom/custom_scripts.js
+++ src/js/custom/custom_scripts.js
@@ -1,8 +1,10 @@
 //custom scripts go here
 $(".navi-button").on("click", function() {
     $(".overlay").addClass("open");
+    return false;
 });
 
 $(".close-button").on("click", function() {
     $(".overlay").removeClass("open");
+    return false;
 });
